2971f462ac5bbde7acb320df35df86f69103e67a
Jonathan Prieto <prieto.jona@gmail.com>
2016-08-11 23:03:01 -0500

    Create PrimaryArithmetic.hs

---
+++ haskell-problems/PrimaryArithmetic/PrimaryArithmetic.hs
@@ -0,0 +1,39 @@
+-- @Author: jonaprieto
+-- @Date:   2016-08-11 23:00:06
+-- @Last Modified by:   jonaprieto
+-- @Last Modified time: 2016-08-11 23:01:16
+
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Main
+    where
+
+import           Control.Monad   (unless)
+import           Data.List.Split (splitOn)
+
+countCarries ∷ Int → Int → Int
+countCarries n m = countCarries' n m 0 0
+
+countCarries' ∷ Int → Int → Int → Int → Int
+countCarries' n m carry carries
+  | n == 0              = carries
+  | n > m               = countCarries' m n carry carries
+  | sumUnits >= 10      = countCarries' newN newM newCarry (carries + 1)
+  | otherwise           = countCarries' newN newM 0 carries
+  where
+    newN, newM, newCarry, sumUnits ∷ Int
+    sumUnits = n `mod` 10 + m `mod` 10 + carry
+    newN = n `div` 10
+    newM = m `div` 10
+    newCarry = sumUnits `div` 10
+
+main ∷ IO ()
+main = do
+  nm ← getLine
+  let [n,m] = map (\x → read x ∷ Int) (splitOn " " nm)
+  unless ((n+m)== 0) $ do
+    case countCarries n m of
+      0 → putStrLn "No carry operation."
+      1 → putStrLn "1 carry operation."
+      c → putStrLn $ show c ++ " carry operations."
+    main
